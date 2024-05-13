348097437fd3d0b2a403e06c68fdcce31e388b50
Lubarskiy Konstantin <lubarskiy.k@gmail.com>
2024-05-13 16:56:06 +0300

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -3,9 +3,9 @@ console.log(`Number of categories ${categories.childElementCount}`);
 const item = document.querySelectorAll('.item');
 let categoriesName = '';
 let categoriesElementCount = '';
-for (let i = 0; i < item.length; i++) {
-  categoriesName = item[i].querySelector('h2').textContent;
+item.forEach(elem => {
+  categoriesName = elem.querySelector('h2').textContent;
   console.log(`Category: ${categoriesName}`);
-  categoriesElementCount = item[i].querySelector('ul').childElementCount;
+  categoriesElementCount = elem.querySelector('ul').childElementCount;
   console.log(`Elements: ${categoriesElementCount}`);
-}
+});
