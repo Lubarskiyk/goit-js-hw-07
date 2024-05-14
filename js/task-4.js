f8676d71f82eb6b3092f13d464efa82dfb1a1a91
Lubarskiy Konstantin <lubarskiy.k@gmail.com>
2024-05-14 16:32:53 +0300

    task-4 done

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,17 @@
+const registerForm = document.querySelector('.login-form');
+registerForm.addEventListener('submit', clickSubmit);
 
+function clickSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const formData = {
+    email: form.elements.email.value,
+    password: form.elements.password.value,
+  };
+
+  if (formData.email === '' || formData.password === '') {
+    return alert('All form fields must be filled in');
+  }
+  console.log(formData);
+  form.reset();
+}
